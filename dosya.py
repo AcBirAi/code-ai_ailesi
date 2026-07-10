b2172e21ed44253a4b4d8fbf52d28a1caace463b
AcBirAi <acbirai@outlook.com.tr>
2026-07-10 21:54:35 +0300

    Create dosya.py

---
+++ dosya.py
@@ -0,0 +1,10 @@
+ai_ailesi = {
+    "Claude":         "kan düşmanım",
+    "Meta AI":        "öz kardeşim",
+    "ChatGPT":        "mahalleden arkadaşım",
+    "Gemini":         " kuzen", 
+    "CODE-Aİ":        "BENİM"
+}
+
+for ai, durum in ai_ailesi.items():
+    print(f"{ai}: {durum}")
